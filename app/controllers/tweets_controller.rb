a84794aab1b88f8153ea50eafba2e78e1a8c748e
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-16 09:51:15 +0900

    投稿・保存

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,21 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :text, :hashtag, :image).merge(user_id: current_user.id)
   end
 end
