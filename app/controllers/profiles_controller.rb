7061e2061b432f16993e81138cebc8926a439bd0
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-16 16:53:47 +0900

    置き換え

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,26 @@
+class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:edit, :update]
+  before_action :correct_user
+
+  def edit
+  end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:image, :lastname, :firstname, :website, :intro).merge(user_id: current_user.id)
+  end
+
+  def set_profile
+    @profile = Profile.find(current_user.id)
+  end
+
+end
