41718f1f309fd0c66da17ca849420c1b9138f67e
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-16 16:38:20 +0900

    プロフィールテーブル作成

--- db/migrate/20221116073312_create_profiles.rb
+++ db/migrate/20221116073312_create_profiles.rb
@@ -1,7 +1,10 @@
 class CreateProfiles < ActiveRecord::Migration[6.0]
   def change
     create_table :profiles do |t|
-
+      t.string :nickname, null: false
+      t.text :information, null: false
+      t.references :user, null: false, foreign_key: true
+      
       t.timestamps
     end
   end
