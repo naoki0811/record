0548ee1b17722ead850d969e61a25e70aedfcf00
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-16 11:20:24 +0900

    編集可能

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :update, :show]
+
   def index
     @tweet = Tweet.all
   end
@@ -17,15 +19,29 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    if @tweet.user_id != current_user.id 
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
   end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:title, :text, :hashtag, :image).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
