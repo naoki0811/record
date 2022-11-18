b81dd3086ea3221a2507abc046bde4e45b459e57
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-18 17:30:33 +0900

    いいね途中　非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :set_tweet, only: [:create, :destroy]
+
   def create
-    tweet = Tweet.find(params[:tweet_id])
     if user_signed_in?
-      favorite = current_user.favorites.new(tweet_id: tweet.id)
+      favorite = current_user.favorites.new(tweet_id: @tweet.id)
       favorite.save
-    else
-      redirect_to tweet_path(tweet)
     end
   end
   
   def destroy
-    tweet = Tweet.find(params[:tweet_id])
     if user_signed_in?
-      favorite = current_user.favorites.find_by(tweet_id: tweet.id)
+      favorite = current_user.favorites.find_by(tweet_id: @tweet.id)
       favorite.destroy
-    else
-      redirect_to tweet_path(tweet)
     end
+  end
+
+  private
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
   end
 end
