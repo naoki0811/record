e74be1b76145dc02d99813050383c3ecd196e157
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-18 09:33:11 +0900

    プロフィール画像表示できた

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c6de3d6b8d15b99a52cd347960cfbd33d1fc42bfc75a4a2eb9fa03d4fdc4c0a844b5fce9646bf2ce0583b05f19620d8227104d9c6e57747e44283081b28ee69d'
