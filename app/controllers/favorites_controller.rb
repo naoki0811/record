3ad3552fbbe4dafd3eb15b8d3982d2de41ee39bf
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-18 14:27:52 +0900

    置き換え

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.new(tweet_id: tweet.id)
     favorite.save
-    redirect_to post_path(tweet)
+    redirect_to tweet_path(tweet)
   end
   
   def destroy
     tweet = Tweet.find(params[:tweet_id])
     favorite = current_user.favorites.find_by(tweet_id: tweet.id)
     favorite.destroy
-    redirect_to post_path(tweet)
+    redirect_to tweet_path(tweet)
   end
 end
