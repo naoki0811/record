755186742aa1e3c475d51bcdb3a3bbd871ceb219
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-18 13:30:09 +0900

    アソシエーション記述

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_one_attached :image
 
   validates :title, presence: true
