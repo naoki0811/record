8948c9ad949ed5d58c444d9a2e2314034f76b10f
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-19 19:25:13 +0900

    コンフリクト解消？

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,27 +1,16 @@
 class FavoritesController < ApplicationController
-  before_action :set_tweet, only: [:create, :destroy]
 
   def create
-    if user_signed_in?
-      favorite = current_user.favorites.new(tweet_id: tweet.id)
-      favorite.save
-    else
-      redirect_to tweet_path(tweet)
-    end
+    tweet = Tweet.find(params[:tweet_id])
+    favorite = current_user.favorites.new(tweet_id: tweet.id)
+    favorite.save
+    redirect_to tweet_path(tweet)
   end
 
   def destroy
-    if user_signed_in?
-      favorite = current_user.favorites.find_by(tweet_id: tweet.id)
-      favorite.destroy
-    else
-      redirect_to tweet_path(tweet)
-    end
-  end
-
-  private
-
-  def set_tweet
     tweet = Tweet.find(params[:tweet_id])
+    favorite = current_user.favorites.find_by(tweet_id: tweet.id)
+    favorite.destroy
+    redirect_to tweet_path(tweet)
   end
 end
