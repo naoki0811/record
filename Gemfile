1cf64bb7a0f3312d6945d17fac676b788237fbcc
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-19 19:04:43 +0900

    変更する

--- Gemfile
+++ Gemfile
@@ -76,5 +76,3 @@ gem "aws-sdk-s3", require: false
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
-
-gem 'jquery-rails'
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,21 +3,25 @@ class FavoritesController < ApplicationController
 
   def create
     if user_signed_in?
-      favorite = current_user.favorites.new(tweet_id: @tweet.id)
+      favorite = current_user.favorites.new(tweet_id: tweet.id)
       favorite.save
+    else
+      redirect_to tweet_path(tweet)
     end
   end
-  
+
   def destroy
     if user_signed_in?
-      favorite = current_user.favorites.find_by(tweet_id: @tweet.id)
+      favorite = current_user.favorites.find_by(tweet_id: tweet.id)
       favorite.destroy
+    else
+      redirect_to tweet_path(tweet)
     end
   end
 
   private
 
   def set_tweet
-    @tweet = Tweet.find(params[:tweet_id])
+    tweet = Tweet.find(params[:tweet_id])
   end
 end
