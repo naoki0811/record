3373bf637043e24bdba3023f00b6d97fa9e8153f
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-20 19:36:06 +0900

    いいね機能　完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "tweets#index"
   resources :tweets do
     resources :comments, only: [:destroy, :create]
-    resources :favorites, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 
   resources :users, only:[:index, :show, :edit, :update]
