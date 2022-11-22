5b1113d5336e7ec37f4463b74bf4e20c81a34b51
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-22 17:00:41 +0900

    テーブル作成

--- db/migrate/20221122075350_create_relationships.rb
+++ db/migrate/20221122075350_create_relationships.rb
@@ -1,10 +1,12 @@
 class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
-      t.references :follower, null: false, foreign_key: true
-      t.references :followed, null: false, foreign_key: true
+      t.references :follower, foreign_key: { to_table: :users }
+      t.references :followed, foreign_key: { to_table: :users }
 
       t.timestamps
+
+      t.index [:follower_id, :followed_id], unique: true
     end
   end
 end
