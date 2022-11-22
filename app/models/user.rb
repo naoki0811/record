fe1d058db27a11f5490fa1d82752c7ddf44051be
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-11-22 17:07:47 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comennts, dependent: :destroy
   attachment :profile_image
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
