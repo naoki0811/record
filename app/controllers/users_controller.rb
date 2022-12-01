a8a4fd8e49439538d74e39e83841642621142d7b
naoki0811 <naoki.k.0811.0811@gmail.com>
2022-12-01 09:21:30 +0900

    変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     favorites= Favorite.where(user_id: @user.id).order("created_at DESC").pluck(:tweet_id)
     @favorite_tweets = Tweet.find(favorites)
+    @tweets = @user.tweets.page(params[:page]).per(4).order('created_at DESC')
   end
 end
 
